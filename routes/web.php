7002a40552e0d560ee649270122a3a6d388c3587
lutfiddin <shamsidinovlutfiddin@gmail.com>
2021-03-27 22:05:11 +0500

    routes crud

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('student');
+// });
+
+Route::get('/', [StudentController::class, 'index']);
+Route::get('/edit/{id}', [StudentController::class, 'edit']);
+Route::get('/show/{id}', [StudentController::class, 'show']);
+Route::get('/create', [StudentController::class, 'create']);
+Route::post('/store', [StudentController::class, 'store']);
+Route::post('/update/{id}', [StudentController::class, 'update']);
